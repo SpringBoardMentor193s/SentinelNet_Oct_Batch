32d4abdc227a0a24d86550b086a777ca92b14b28
Soha Parween <sohabeby786@gmail.com>
2025-10-21 22:12:50 +0530

    Add main.py file

---
+++ OneDrive/New folder/OneDrive/Desktop/SentinelNet/main.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import pandas as pd
+import matplotlib as plt
+import seaborn as sns
+
+data = pd.read_csv("kdd_test.csv")
+
+print("Original Dataset:")
+print(data.head())
+
+column_to_delete = "num_root"
+if column_to_delete in data.columns:
+    data.drop(column_to_delete, axis=1, inplace=True)
+    print(f"\nColumn '{column_to_delete}' deleted successfully.")
+else:
+    print(f"\nColumn '{column_to_delete}' not found in dataset.")
+
+print("\nDataset after deletion:")
+print(data.head())
+
+data.to_csv("updated_kdd_test.csv", index=False)
+print("\nUpdated dataset saved as 'updated_kdd_test.csv' in the same folder.")
