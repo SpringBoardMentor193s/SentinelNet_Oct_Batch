09f1cee9d71fa5b17688fe56d99561737cd8699f
Aditi Deshpande <aditideshpande3004@gmail.com>
2025-11-03 15:47:12 +0530

    Completed the EDA Process and built the final data sets for model building process

--- dataset1_info.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-
-#loading the dataset
-df = pd.read_csv('Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv')
-print("Dataset 1 loaded successfully...")
--- dataset2_info.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-
-#loading the dataset
-df = pd.read_csv('kdd_train.csv')
-print("Dataset 2 loaded successfully...")
